9ea92aa50a7d2f5f0cb05301cdaffc9c1cf958f5
Marce Ramirez <marceram87@gmail.com>
2019-07-28 16:05:28 +0200

    Styling profile summary section

--- routes/profile.js
+++ routes/profile.js
@@ -9,33 +9,46 @@ const genreList = require('../helpers/genres');
 const { signinRequired } = require('../middlewares/authMiddlewares');
 
 router.get('/', signinRequired, (req, res, next) => {
-  res.render('profile');
+  const hasAddress =
+    req.session.currentUser.userType === 'stage' ? 'Has address' : null;
+  res.render('profile', { hasAddress });
 });
 
 router.get('/edit', signinRequired, (req, res, next) => {
   res.render('editProfile', { genreList });
 });
 
-router.post('/edit', parser.single('photo'), signinRequired, async (req, res, next) => {
-  console.log('User:', req.session.currentUser);
-  try {
-    const { name, tagline, location, description, address, genres } = req.body;
-    console.log('Body', req.body);
-    const profilePicture = req.file
-      ? req.file.secure_url
-      : req.session.currentUser.profilePicture;
-    const user = req.session.currentUser;
-    const type = user.userType === 'band' ? Band : Stage;
-    const newUser = await type.findByIdAndUpdate(
-      user._id,
-      { $set: { name, profilePicture } },
-      { new: true }
-    );
-    req.session.currentUser = newUser;
-    res.redirect('/profile');
-  } catch (error) {
-    next(error);
+router.post(
+  '/edit',
+  parser.single('photo'),
+  signinRequired,
+  async (req, res, next) => {
+    try {
+      const {
+        name,
+        tagline,
+        location,
+        description,
+        address,
+        genres
+      } = req.body;
+      console.log('Body', req.body);
+      const profilePicture = req.file
+        ? req.file.secure_url
+        : req.session.currentUser.profilePicture;
+      const user = req.session.currentUser;
+      const type = user.userType === 'band' ? Band : Stage;
+      const newUser = await type.findByIdAndUpdate(
+        user._id,
+        { $set: { name, profilePicture } },
+        { new: true }
+      );
+      req.session.currentUser = newUser;
+      res.redirect('/profile');
+    } catch (error) {
+      next(error);
+    }
   }
-});
+);
 
 module.exports = router;
