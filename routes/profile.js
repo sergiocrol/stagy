098af871a13b169ff4e92bc097f143dc835f9de7
Marce Ramirez <marceram87@gmail.com>
2019-07-28 13:35:04 +0200

    Style error messages. Start with profile edit

--- routes/profile.js
+++ routes/profile.js
@@ -4,22 +4,23 @@ const parser = require('../config/cloudinary');
 
 const Band = require('../models/Band');
 const Stage = require('../models/Stage');
+const genreList = require('../helpers/genres');
 
 const { signinRequired } = require('../middlewares/authMiddlewares');
 
 router.get('/', signinRequired, (req, res, next) => {
-  console.log(req.session.currentUser);
   res.render('profile');
 });
 
 router.get('/edit', signinRequired, (req, res, next) => {
-  res.render('editProfile');
+  res.render('editProfile', { genreList });
 });
 
 router.post('/edit', parser.single('photo'), signinRequired, async (req, res, next) => {
   console.log('User:', req.session.currentUser);
   try {
-    const { name } = req.body;
+    const { name, tagline, location, description, address, genres } = req.body;
+    console.log('Body', req.body);
     const profilePicture = req.file
       ? req.file.secure_url
       : req.session.currentUser.profilePicture;
