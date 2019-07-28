00babb4391c731e227e2909d76e08a287c9c237a
Sergio Cordero <sergio.crol@gmail.com>
2019-07-28 16:03:10 +0200

    Image selection changed

--- routes/profile.js
+++ routes/profile.js
@@ -9,17 +9,21 @@ const genreList = require('../helpers/genres');
 const { signinRequired } = require('../middlewares/authMiddlewares');
 
 router.get('/', signinRequired, (req, res, next) => {
-  res.render('profile');
+  const hasAddress =
+    req.session.currentUser.userType === 'stage' ? 'Has address' : null;
+  res.render('profile', { hasAddress });
 });
 
 router.get('/edit', signinRequired, (req, res, next) => {
-  res.render('editProfile', { genreList });
+  const hasAddress =
+  req.session.currentUser.userType === 'stage' ? 'Has address' : null;
+  res.render('editProfile', { genreList, hasAddress });
 });
 
-router.post('/edit', parser.single('photo'), signinRequired, async (req, res, next) => {
+router.post('/edit', parser.single('edit-photo'), signinRequired, async (req, res, next) => {
   console.log('User:', req.session.currentUser);
   try {
-    const { name, tagline, location, description, address, genres } = req.body;
+    const { name, tagLine, location, description, genres, address } = req.body;
     console.log('Body', req.body);
     const profilePicture = req.file
       ? req.file.secure_url
@@ -28,7 +32,7 @@ router.post('/edit', parser.single('photo'), signinRequired, async (req, res, ne
     const type = user.userType === 'band' ? Band : Stage;
     const newUser = await type.findByIdAndUpdate(
       user._id,
-      { $set: { name, profilePicture } },
+      { $set: { name, tagLine, location, description, genres, profilePicture, address } },
       { new: true }
     );
     req.session.currentUser = newUser;
