69ed2e146749276f1c12e9e474f5138b5e729d42
Marce Ramirez <marceram87@gmail.com>
2019-07-29 12:34:48 +0200

    Create users page and router

--- helpers/bandSeeds.js
+++ helpers/bandSeeds.js
@@ -14,60 +14,52 @@ const bands = [
   {
     name: 'Aerosmith',
     email: 'aerosmith@mail.com',
-    password: '$2b$10$52sNsu0cA/53KjiIM7aOOuDZyuqN2n5Km0VcG7E3TK9icFbZ2ByMW',
+    password: '$2b$10$52sNsu0cA',
     location: 'Barcelona',
     genre: ['Rock'],
-    otherLocations: [],
-    rating: 0,
-    reviewers: 0,
+    rating: 4.7,
+    reviewers: 27,
     description: 'American rock band formed in Boston',
     tagLine: 'We are Aerosmith!',
-    profilePicture: '',
     userType: 'band'
   },
   {
     name: 'The Rolling Stones',
     email: 'rollingstones@mail.com',
-    password: '$2b$10$52sNsu0cA/53KjiIM7aOOuDZyuqN2n5Km0VcG7E3TK9icFbZ2ByMW',
+    password: '$2b$10$52sNsu0cA',
     location: 'Bilbao',
     genre: ['Rock'],
-    otherLocations: [],
-    rating: 0,
-    reviewers: 0,
+    rating: 4.2,
+    reviewers: 58,
     description:
       'The Rolling Stones are an English rock band formed in London in 1962. The first stable line-up consisted of bandleader Brian Jones (guitar, harmonica, keyboards), Mick Jagger (lead vocals, harmonica), Keith Richards (guitar, vocals), Bill Wyman (bass), Charlie Watts (drums), and Ian Stewart (piano)',
     tagLine: 'We are The Rolling Stones!',
-    profilePicture: '',
     userType: 'band'
   },
   {
     name: 'Grupo Niche',
     email: 'gruponiche@mail.com',
-    password: '$2b$10$52sNsu0cA/53KjiIM7aOOuDZyuqN2n5Km0VcG7E3TK9icFbZ2ByMW',
+    password: '$2b$10$52sNsu0cA',
     location: 'Barcelona',
     genre: ['Salsa'],
-    otherLocations: [],
-    rating: 0,
-    reviewers: 0,
+    rating: 4.8,
+    reviewers: 61,
     description:
       'Grupo Niche is a salsa group founded in 1978 in Cali, Colombia. Currently based in Cali, Colombia',
     tagLine: 'We are Grupo Niche!',
-    profilePicture: '',
     userType: 'band'
   },
   {
     name: 'Grupo Galé',
     email: 'grupogale@mail.com',
-    password: '$2b$10$52sNsu0cA/53KjiIM7aOOuDZyuqN2n5Km0VcG7E3TK9icFbZ2ByMW',
+    password: '$2b$10$52sNsu0cA',
     location: 'Bilbao',
     genre: ['Salsa'],
-    otherLocations: [],
-    rating: 0,
-    reviewers: 0,
+    rating: 3.5,
+    reviewers: 18,
     description:
       'Grupo Galé is a Colombian salsa music band Their album Auténtico was nominated for Latin Grammy Award for Best Salsa Album at the Latin Grammy Awards of 2008.',
     tagLine: 'We are Grupo Galé!',
-    profilePicture: '',
     userType: 'band'
   }
 ];
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const router = express.Router();
 
+const Band = require('../models/Band');
+const Stage = require('../models/Stage');
+
 /* GET users listing. */
 router.get('/', (req, res, next) => {
-  res.send('respond with a resource');
+  res.redirect('/');
+});
+
+router.get('/:id', async (req, res, next) => {
+  const type = req.query.type === 'band' ? Band : Stage;
+  const { id } = req.params;
+  const allowRequest = req.query.type === 'band' ? 'Allow Requests' : '';
+  console.log(req.query.type, allowRequest);
+  try {
+    const user = await type.findById(id);
+    res.render('user', { user, allowRequest });
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
